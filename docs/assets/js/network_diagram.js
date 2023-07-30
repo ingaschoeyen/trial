89b116e12f70f48754b61cecd21e453a9c1002df
inga420 <60630082+inga420@users.noreply.github.com>
2023-07-30 17:21:23 +0200

    fixed filepath

--- docs/assets/js/network_diagram.js
+++ docs/assets/js/network_diagram.js
@@ -1,4 +1,4 @@
-var filepath = "json/References.json";
+var filepath = "assets/json/References.json";
 var network_div = document.getElementById("network");
 var options_div = document.getElementById("options");
 var references_div = document.getElementById("references");
